c963a7f8f3d2319279f0b7cbe670e43d04152acc
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-07-11 17:12:10 +0530

    form

--- src/pages/AddMemory.jsx
+++ src/pages/AddMemory.jsx
@@ -1,59 +1,151 @@
-import { Box, FormControl, FormHelperText, IconButton, InputAdornment, InputLabel, MenuItem, Paper, Select, Stack, TextField, Typography } from "@mui/material"
-import { useEffect, useState } from "react"
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import SentimentVerySatisfiedIcon from '@mui/icons-material/SentimentVerySatisfied';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 
-import { TextareaAutosize } from '@mui/base';
+
+export default function AddMemory() {
+
+    const [data, setData] = React.useState({
+        name: "",
+        mood: "happy",
+        handle: "anonymous",
+        desc: "",
+        password: ""
+    })
 
 
-const AddMemory = () => {
-
-    useEffect(() => {
-
-        document.title = "Add Memory | GEET"
-    }, [])
-
-    const [age, setAge] = useState('');
-
-    const handleChangeAge = (event) => {
-        setAge(event.target.value);
+    const handleSubmit = (event) => {
+        console.log(data)
     };
 
+    const handleOnChange = (e) => {
+        setData({ ...data, [e.target.name]: e.target.value })
+    }
 
     return (
         <>
-            <div>
-                <FormControl>
-                    <Typography variant="h5" >Add Memory</Typography>
-                    <Stack>
-                        <TextField sx={{ mt: 2 }} label="Title" type="text" />
-                        <TextField sx={{ mt: 2 }} label="Description" type="text" />
-                        <Stack direction="row" spacing={3} sx={{ mt: 2 }}>
-                            <TextField sx={{}} label="Handle" type="text" />
-                            <TextareaAutosize />
-                        </Stack>
-                        <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
-                            <Box>
-                                <InputLabel id="demo-simple-select-label" color="secondary">Age</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    value={age}
-                                    label="Age"
-                                    onChange={handleChangeAge}
-                                    size="small"
-                                >
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </Box>
-                        </Stack>
-                    </Stack>
-                </FormControl>
-            </div>
-        </>
-    )
-}
+            <Container component="main" maxWidth="xs" sx={{ mb: 6 }}>
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <SentimentVerySatisfiedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Add Memory
+                    </Typography>
+                    <Box component="div" sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <FormControl fullWidth>
+                                    <InputLabel id="demo-simple-select-label">Mood</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={data.mood}
+                                        label="Mood"
+                                        name="mood"
+                                        onChange={handleOnChange}
+                                    >
+                                        <MenuItem value={"happy"}>Happy</MenuItem>
+                                        <MenuItem value={"sad"}>Sad</MenuItem>
+                                        <MenuItem value={"anger"}>Anger</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="lastName"
+                                    label="Handle"
+                                    name="handle"
+                                    placeholder={data.handle}
+                                    title="leave black for anonimity"
+                                    onChange={handleOnChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="name"
+                                    label="Title"
+                                    type="text"
+                                    id="title"
+                                    value={data.name}
+                                    onChange={handleOnChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <textarea rows="10" cols="45" style={{ border: "1px solid white", backgroundColor: "#121212", color: "white", outline: "none", padding: "4px 5px" }} placeholder='Add Description about the memory' value={data.desc} onChange={handleOnChange} name="desc" />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    onChange={handleOnChange}
+                                />
+                            </Grid>
 
-export default AddMemory
+                        </Grid>
+                        <Grid container
+                            direction="row"
+                            justifyContent="space-evenly"
+                            alignItems="center"
+                            spacing="4"
+                        >
+                            <Grid item xs={4} >
+                                <Button
+                                    variant="contained"
+                                    component="label"
+                                >
+                                    Upload File
+                                    <input
+                                        type="file"
+                                        accept='.png,.jpg,.jpeg'
+                                        hidden
+                                    />
+                                </Button>
+                            </Grid>
+                            <Grid items xs={8}>
+                                <Button
+                                    type="submit"
+                                    onClick={handleSubmit}
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                >
+                                    Add Memory
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </>
+
+    );
+}
