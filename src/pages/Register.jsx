1059ab6d0747fbffca2e107b132b9c1a905d53a8
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-07-20 13:21:57 +0530

    working

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,21 +14,46 @@ import { useNavigate } from 'react-router-dom';
 export default function Register() {
     let navigate = useNavigate();
 
+    const [data, setData] = useState({
+        username: "",
+        email: "",
+        password: ""
+    })
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
+    async function postData(url = "") {
+        console.log("URL: " + `${process.env.REACT_APP_BACKEND_URL}/${url}`)
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/${url}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: data.username,
+                email: data.email,
+                password: data.password
+            }),
         });
-    };
+        let res = await response.json();
+        // console.log(res +" data")
+        return res;
+    }
+
+    const formSubit = async () => {
+        let res = await postData("auth/register");
+        console.log("Res: " + res.message);
+        // console.log(data)
+    }
+
+    const handleChange = (e) => {
+        setData({ ...data, [e.target.name]: e.target.value })
+    }
 
     useEffect(() => {
         if (localStorage.getItem("user")) {
             navigate("/")
         }
     }, [])
+
 
     return (
         <Container component="main" maxWidth="xs" style={{ marginBottom: "12rem" }}>
@@ -47,16 +72,19 @@ export default function Register() {
                 <Typography component="h1" variant="h5">
                     Sign up
                 </Typography>
-                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                <Box component="form" noValidate sx={{ mt: 3 }}>
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
                             <TextField
                                 required
                                 fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
+                                id="username"
+                                label="Username"
+                                name="username"
+                                type='text'
+                                value={data.username}
+                                onChange={handleChange}
+
                             />
                         </Grid>
                         <Grid item xs={12}>
@@ -66,7 +94,10 @@ export default function Register() {
                                 id="email"
                                 label="Email Address"
                                 name="email"
+                                type="text"
                                 autoComplete="email"
+                                value={data.email}
+                                onChange={handleChange}
                             />
                         </Grid>
                         <Grid item xs={12}>
@@ -78,12 +109,14 @@ export default function Register() {
                                 type="password"
                                 id="password"
                                 autoComplete="new-password"
+                                value={data.password}
+                                onChange={handleChange}
                             />
                         </Grid>
 
                     </Grid>
                     <Button
-                        type="submit"
+                        onClick={formSubit}
                         fullWidth
                         variant="contained"
                         sx={{ mt: 3, mb: 2 }}
@@ -92,7 +125,7 @@ export default function Register() {
                     </Button>
                     <Grid container justifyContent="flex-end">
                         <Grid item>
-                            <Link href="#" variant="body2">
+                            <Link href="/login" variant="body2">
                                 Already have an account? Sign in
                             </Link>
                         </Grid>
